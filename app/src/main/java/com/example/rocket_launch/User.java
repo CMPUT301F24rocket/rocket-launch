57f607aeb70049a3a299adc74ece0c7f4901767f
Chiku6167 <chikupii73489@gmail.com>
2024-10-27 16:19:09 -0600

    added to User class

--- app/src/main/java/com/example/rocket_launch/User.java
+++ app/src/main/java/com/example/rocket_launch/User.java
@@ -6,22 +6,44 @@ import android.media.Image;
 
 public class User {
 
+    //Android ID
     private String android_id;
-    private Roles roles;
-    private String email;
-    private Location location;
+
+    //User Profile Information
+    private String userName;
+    private String userEmail;
+    private String userPhoneNumber;
     private Image profilePhoto;
+
+    private Location location;
+    private Roles roles;
     private boolean notificationPreference;
 
+    //get user profile information
+    String getUserName() {return this.userName;}
+    String getUserEmail() {return this.userEmail;}
+    String getUserPhoneNumber() {return this.userPhoneNumber;}
+    Image getProfilePhoto() {return this.profilePhoto;}
 
-    public User() {
-        this.roles = new Roles();
-    }
+    //set user profile information
+    public void setUserName(String userName) {this.userName = userName;}
+    public void setUserEmail(String userEmail) {this.userEmail = userEmail;}
+    public void setUserPhoneNumber(String userPhoneNumber) {this.userPhoneNumber = userPhoneNumber;}
+    public void setProfilePhoto(Image profilePhoto){this.profilePhoto = profilePhoto;}
 
+    //user Location Data
+
+
+    //Android ID
+    String getAndroid_id() {return this.android_id;}
     public void setAndroid_id(String android_id) {
         this.android_id = android_id;
     }
 
+    //User Roles
+    public User() {
+        this.roles = new Roles();
+    }
     public Roles getRoles() {
         return roles;
     }
