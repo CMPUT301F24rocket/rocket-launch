a995aac62e56fa7815df7766a5578089b3864ca0
Nathan Lapak <nathan.lapak@gmail.com>
2024-11-04 07:59:48 -0700

    Test

--- app/src/main/java/com/example/rocket_launch/MainActivity.java
+++ app/src/main/java/com/example/rocket_launch/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.rocket_launch;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     User user;
     UsersDB usersDB;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,32 +34,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
         usersDB = new UsersDB(); // Load user database
-        String androidId = "your_device_id"; // Replace with actual device ID
+
+        //Get Android Device ID
+        String androidID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);;
+
+        // Get Firebase user
+        usersDB.getUser(androidID, new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    user = documentSnapshot.toObject(User.class);
+                    checkUserRole(user);
+                } else {
+                    user = new User();
+                    user.setAndroid_id(androidID); //set Android ID for new user
+                    new NewUserFragment(user, usersDB).show(getSupportFragmentManager(), "Create New User");
+                    usersDB.addUser(androidID, user);
+                }
+            }
+        }, e -> {
+            Log.w("Firebase", "Error getting user", e);
+        });
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
         bottomNav.setSelectedItemId(R.id.navigation_home);
         bottomBarNavigation(bottomNav);
-
-        Button get_started_button = findViewById(R.id.get_started);
-
-        get_started_button.setOnClickListener(v -> {
-            // Get Firebase user
-            usersDB.getUser(androidId, new OnSuccessListener<DocumentSnapshot>() {
-                @Override
-                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                    if (documentSnapshot.exists()) {
-                        user = documentSnapshot.toObject(User.class);
-                        checkUserRole(user);
-                    } else {
-                        user = new User();
-                        new NewUserFragment(user).show(getSupportFragmentManager(), "Create New User");
-                    }
-                }
-            }, e -> {
-                Log.w("Firebase", "Error getting user", e);
-            });
-
-        });
     }
 
     private void checkUserRole(User user) {
