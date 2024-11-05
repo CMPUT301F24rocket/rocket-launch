b3d2a2f0c6268e879e772adc7f6fb983398972fc
Nathan Lapak <nathan.lapak@gmail.com>
2024-11-05 13:48:15 -0700

    nathan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,11 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+
+    // Bumptech glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+
     //firebase
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-analytics")
