556b5fc8d60c598d0f9106ac28d4aa7535e62d5e
Chiku6167 <chikupii73489@gmail.com>
2024-11-07 17:04:51 -0700

    Created More Organizer Fragments

---
+++ app/src/main/java/com/example/rocket_launch/organizer_event_details/OrganizerViewQrCodeFragment.java
@@ -0,0 +1,41 @@
+package com.example.rocket_launch.organizer_event_details;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rocket_launch.R;
+
+
+public class OrganizerViewQrCodeFragment extends Fragment {
+
+
+    public OrganizerViewQrCodeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_organizer_view_qr_code, container, false);
+
+        return view;
+    }
+
+    // Close the fragment and return to the Created Activities view
+    private void closeFragment() {
+        requireActivity().getSupportFragmentManager().popBackStack();
+    }
+}
