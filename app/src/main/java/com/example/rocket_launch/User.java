d612548754c609f782cee1b7593067d036b5d133
Kaiden <65012097+kaiden-keane@users.noreply.github.com>
2024-11-07 20:33:45 -0700

    formatted User

--- app/src/main/java/com/example/rocket_launch/User.java
+++ app/src/main/java/com/example/rocket_launch/User.java
@@ -23,82 +23,64 @@ public class User {
     private Roles roles;
     private Boolean notificationPreferences;
     private List<String> eventsJoined;
+    private List<String> eventsCreated;
 
     private List<String> notifications;
 
-    public List<String> getNotifications() {return notifications;}
 
-    public void setNotifications(List<String> notifications) {
-        this.notifications = notifications;
-    }
-
-    public void addNotification(String notification){
-        notifications.add(notification);
-    }
-
-    public void addEvent(String eventID){
-        eventsJoined.add(eventID);
-    }
-
-
-    public List<String> getEventsJoined() {
-        return eventsJoined;
-    }
-
-    //get user profile information
-    String getUserName() {return this.userName;}
-    String getUserEmail() {return this.userEmail;}
-    String getUserPhoneNumber() {return this.userPhoneNumber;}
-    Image getProfilePhoto() {return this.profilePhoto;}
-    String getUserFacility(){return  this.userFacility;}
-
-    //set user profile information
-    public void setUserName(String userName) {this.userName = userName;}
-    public void setUserEmail(String userEmail) {this.userEmail = userEmail;}
-    public void setUserPhoneNumber(String userPhoneNumber) {this.userPhoneNumber = userPhoneNumber;}
-    public void setProfilePhoto(Image profilePhoto){this.profilePhoto = profilePhoto;}
-    public void setUserFacility(String userFacility){this.userFacility = userFacility;}
-
-    //user Location Data
-
-
-    //Android ID
-    String getAndroid_id() {return this.android_id;}
-    public void setAndroid_id(String android_id) {
-        this.android_id = android_id;
-    }
-
-    //User Roles
     public User() {
         this.roles = new Roles();
         this.eventsJoined = new ArrayList<>();
         this.notifications = new ArrayList<>();
     }
-    public Roles getRoles() {
-        return roles;
-    }
 
-    public void setRoles(Roles roles) {
-        this.roles = roles;
-    }
+    // Username
+    String getUserName() {return this.userName;}
+    public void setUserName(String userName) {this.userName = userName;}
 
-    public Boolean isEntrant() {
-        return this.roles.isEntrant();
-    }
+    // user email
+    String getUserEmail() {return this.userEmail;}
+    public void setUserEmail(String userEmail) {this.userEmail = userEmail;}
 
-    public Boolean isOrganizer() {
-        return this.roles.isOrganizer();
-    }
+    // user phone number
+    String getUserPhoneNumber() {return this.userPhoneNumber;}
+    public void setUserPhoneNumber(String userPhoneNumber) {this.userPhoneNumber = userPhoneNumber;}
 
-    public Boolean isAdmin() {
-        return this.roles.isAdmin();
-    }
+    // profile photo
+    Image getProfilePhoto() {return this.profilePhoto;}
+    public void setProfilePhoto(Image profilePhoto){this.profilePhoto = profilePhoto;}
 
-    public Boolean getNotificationPreferences() {
-        return notificationPreferences;
-    }
+    // facility
+    String getUserFacility(){return  this.userFacility;}
+    public void setUserFacility(String userFacility){this.userFacility = userFacility;}
 
-    public void setNotificationPreferences(Boolean notificationPreferences) {
-        this.notificationPreferences = notificationPreferences;
-    }
+    // android id
+    String getAndroid_id() {return this.android_id;}
+    public void setAndroid_id(String android_id) {this.android_id = android_id;}
+
+    // roles
+    public Roles getRoles() {return roles;}
+    public void setRoles(Roles roles) {this.roles = roles;}
+    public Boolean isEntrant() {return this.roles.isEntrant();}
+    public Boolean isOrganizer() {return this.roles.isOrganizer();}
+    public Boolean isAdmin() {return this.roles.isAdmin();}
+
+    // notifications
+    public List<String> getNotifications() {return notifications;}
+    public void setNotifications(List<String> notifications) {this.notifications = notifications;}
+    public void addNotification(String notification){notifications.add(notification);}
+
+    // notification preferences
+    public Boolean getNotificationPreferences() {return notificationPreferences;}
+    public void setNotificationPreferences(Boolean notificationPreferences) {this.notificationPreferences = notificationPreferences;}
+
+    // events joined
+    public List<String> getEventsJoined() {return eventsJoined;}
+    public void setEventsJoined(List<String> eventsJoined) {this.eventsJoined = eventsJoined;}
+    public void addJoinedEvent(String id) {eventsJoined.add(id);};
+
+    // events created
+    public List<String> getEventsCreated() {return eventsCreated;}
+    public void setEventsCreated(List<String> eventsCreated) {this.eventsCreated = eventsCreated;}
+    public void addCreatedEvent(String id) {eventsCreated.add(id);};
 }
