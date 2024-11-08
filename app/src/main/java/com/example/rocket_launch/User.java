b51d9bdaad40cb1b41cee60cfbfb53b77675f8eb
Kaiden Keane <kjkeane88@gmail.com>
2024-11-08 10:11:49 -0700

    reformatted user

--- app/src/main/java/com/example/rocket_launch/User.java
+++ app/src/main/java/com/example/rocket_launch/User.java
@@ -24,70 +24,130 @@ public class User {
     private List<String> notifications;
 
     // event data
-    private List<String> eventsJoined;
+    private List<String> eventsRegistered;
     private List<String> eventsWaitlisted;
     private List<String> eventsCreated;
 
 
     public User() {
         this.roles = new Roles();
-        this.eventsJoined = new ArrayList<>();
+        this.eventsRegistered = new ArrayList<>();
         this.eventsCreated = new ArrayList<>();
         this.notifications = new ArrayList<>();
     }
 
     // Username
-    String getUserName() {return this.userName;}
-    public void setUserName(String userName) {this.userName = userName;}
+    String getUserName() {
+        return this.userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
     // user email
-    String getUserEmail() {return this.userEmail;}
-    public void setUserEmail(String userEmail) {this.userEmail = userEmail;}
+    String getUserEmail() {
+        return this.userEmail;
+    }
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
 
     // user phone number
-    String getUserPhoneNumber() {return this.userPhoneNumber;}
-    public void setUserPhoneNumber(String userPhoneNumber) {this.userPhoneNumber = userPhoneNumber;}
+    String getUserPhoneNumber() {
+        return this.userPhoneNumber;
+    }
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
 
     // profile photo
-    Image getProfilePhoto() {return this.profilePhoto;}
-    public void setProfilePhoto(Image profilePhoto){this.profilePhoto = profilePhoto;}
+    Image getProfilePhoto() {
+        return this.profilePhoto;
+    }
+    public void setProfilePhoto(Image profilePhoto){
+        this.profilePhoto = profilePhoto;
+    }
 
     // facility
-    String getUserFacility(){return  this.userFacility;}
-    public void setUserFacility(String userFacility){this.userFacility = userFacility;}
+    String getUserFacility(){
+        return this.userFacility;
+    }
+    public void setUserFacility(String userFacility){
+        this.userFacility = userFacility;
+    }
 
     // android id
-    String getAndroid_id() {return this.android_id;}
-    public void setAndroid_id(String android_id) {this.android_id = android_id;}
+    String getAndroid_id() {
+        return this.android_id;
+    }
+    public void setAndroid_id(String android_id) {
+        this.android_id = android_id;
+    }
 
     // roles
-    public Roles getRoles() {return roles;}
-    public void setRoles(Roles roles) {this.roles = roles;}
+    public Roles getRoles() {
+        return roles;
+    }
+    public void setRoles(Roles roles) {
+        this.roles = roles;
+    }
 
     // notifications
-    public List<String> getNotifications() {return notifications;}
-    public void setNotifications(List<String> notifications) {this.notifications = notifications;}
-    public void addNotification(String notification){notifications.add(notification);}
+    public List<String> getNotifications() {
+        return notifications;
+    }
+    public void setNotifications(List<String> notifications) {
+        this.notifications = notifications;
+    }
+    public void addNotification(String notification){
+        notifications.add(notification);
+    }
 
     // notification preferences
-    public Boolean getNotificationPreferences() {return notificationPreferences;}
-    public void setNotificationPreferences(Boolean notificationPreferences) {this.notificationPreferences = notificationPreferences;}
+    public Boolean getNotificationPreferences() {
+        return notificationPreferences;
+    }
+    public void setNotificationPreferences(Boolean notificationPreferences) {
+        this.notificationPreferences = notificationPreferences;
+    }
 
     // events joined
-    public List<String> getEventsJoined() {return eventsJoined;}
-    public void setEventsJoined(List<String> eventsJoined) {this.eventsJoined = eventsJoined;}
-    public void addJoinedEvent(String id) {eventsJoined.add(id);};
-    public void removeJoinedEvent(String id) {eventsJoined.remove(id);}
+    public List<String> getEventsRegistered() {
+        return eventsRegistered;
+    }
+    public void setEventsRegistered(List<String> eventsRegistered) {
+        this.eventsRegistered = eventsRegistered;
+    }
+    public void addJoinedEvent(String id) {
+        eventsRegistered.add(id);
+    };
+    public void removeJoinedEvent(String id) {
+        eventsRegistered.remove(id);
+    }
 
     // events created
-    public List<String> getEventsCreated() {return eventsCreated;}
-    public void setEventsCreated(List<String> eventsCreated) {this.eventsCreated = eventsCreated;}
-    public void addCreatedEvent(String id) {eventsCreated.add(id);};
-    public void removeCreatedEvent(String id) {eventsCreated.remove(id);}
+    public List<String> getEventsCreated() {
+        return eventsCreated;
+    }
+    public void setEventsCreated(List<String> eventsCreated) {
+        this.eventsCreated = eventsCreated;}
+    public void addCreatedEvent(String id) {eventsCreated.add(id);
+    };
+    public void removeCreatedEvent(String id) {
+        eventsCreated.remove(id);
+    }
 
     // events waitlisted
-    public List<String> getEventsWaitlisted() {return eventsWaitlisted;}
-    public void setEventsWaitlisted(List<String> eventsWaitlisted) {this.eventsWaitlisted = eventsWaitlisted;}
-    public void addWaitlistEvent(String id) {eventsWaitlisted.add(id);}
-    public void removeWaitlistEvent(String id) {eventsWaitlisted.remove(id);}
+    public List<String> getEventsWaitlisted() {
+        return eventsWaitlisted;
+    }
+    public void setEventsWaitlisted(List<String> eventsWaitlisted) {
+        this.eventsWaitlisted = eventsWaitlisted;
+    }
+    public void addWaitlistEvent(String id) {
+        eventsWaitlisted.add(id);
+    }
+    public void removeWaitlistEvent(String id) {
+        eventsWaitlisted.remove(id);
+    }
 }
