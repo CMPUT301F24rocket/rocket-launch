f70e81f58100f05fdc501f81d7d2fb2352f2558a
Kaiden <65012097+kaiden-keane@users.noreply.github.com>
2024-11-28 18:20:31 -0700

    added invite notifications

--- app/src/main/java/com/example/rocket_launch/InviteNotification.java
+++ app/src/main/java/com/example/rocket_launch/InviteNotification.java
@@ -1,2 +1,28 @@
-package com.example.rocket_launch;public class InviteNotification {
+package com.example.rocket_launch;
+
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
+import android.provider.Settings;
+
+public class InviteNotification extends Notification {
+
+    public String eventId;
+    private EventsDB eventsDB;
+
+    InviteNotification() {
+        eventsDB = new EventsDB();
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public void joinEvent(String userId) {
+        eventsDB.addUserToRegisteredList(eventId, userId);
+        eventsDB.removeUserFromInvitedList(eventId, userId);
+    }
 }
