006ca7c5255e0fab9d4e9601f17c45d2513c378d
Kaiden Keane <kjkeane88@gmail.com>
2024-11-30 01:27:16 -0700

    javadocs

--- app/src/main/java/com/example/rocket_launch/NotificationHandler.java
+++ app/src/main/java/com/example/rocket_launch/NotificationHandler.java
@@ -7,12 +7,12 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-
 import java.util.List;
 import java.util.Map;
 
 /**
  * intermediate class between receiving and showing a notification
+ * Author: Kaiden
  */
 public class NotificationHandler {
     CollectionReference notificationCollectionRef;
@@ -23,6 +23,7 @@ public class NotificationHandler {
 
     /**
      * initialize local notification handler
+     * Author: Kaiden
      * @param context
      *  the application, particularly mainActivity
      * @param userId
@@ -41,6 +42,7 @@ public class NotificationHandler {
 
     /**
      * start notification listener to respond to new notifications
+     * Author: Kaiden
      */
     private void startListener() {
         notificationCollectionRef.addSnapshotListener((querySnapshot, e) -> {
@@ -55,6 +57,7 @@ public class NotificationHandler {
 
     /**
      * handles and sends out all new notifications contained in querySnapshot
+     * Author: Kaiden
      * @param querySnapshot
      *  query of all new notifications in collection
      */
