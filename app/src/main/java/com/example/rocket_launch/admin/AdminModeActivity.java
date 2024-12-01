3b0868cd2ad98f834b8ad5db5ff06287a3d66dd9
Chiku6167 <chikupii73489@gmail.com>
2024-12-01 13:43:09 -0700

    added button navigability between main activity & admin activity

--- app/src/main/java/com/example/rocket_launch/admin/AdminModeActivity.java
+++ app/src/main/java/com/example/rocket_launch/admin/AdminModeActivity.java
@@ -1,11 +1,24 @@
 package com.example.rocket_launch.admin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.rocket_launch.MainActivity;
 import com.example.rocket_launch.R;
+import com.example.rocket_launch.Roles;
+import com.example.rocket_launch.SelectRolesFragment;
+import com.example.rocket_launch.StartUpFragment;
+import com.example.rocket_launch.User;
+import com.example.rocket_launch.UserHomepageFragment;
+import com.example.rocket_launch.UsersDB;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -14,14 +27,27 @@ import com.google.android.material.tabs.TabLayoutMediator;
  * Author: Pouyan
  */
 public class AdminModeActivity extends AppCompatActivity {
+    private UsersDB usersDB;
+    private User user;
+    private ImageButton mainActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        usersDB = new UsersDB();
+        user = new User();
 
         TabLayout tabLayout = findViewById(R.id.admin_tab_layout);
         ViewPager2 viewPager = findViewById(R.id.admin_view_pager);
+        mainActivityButton = findViewById(R.id.back_to_main_activity_button);
+
+        getUser();
+        mainActivityButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
 
         AdminModePagerAdapter adapter = new AdminModePagerAdapter(this);
         viewPager.setAdapter(adapter);
@@ -35,5 +61,24 @@ public class AdminModeActivity extends AppCompatActivity {
                 case 4: tab.setText("Facilities"); break;
             }
         }).attach();
+    }
+
+    private void getUser(){
+        String androidID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        usersDB.getUser(androidID, new OnSuccessListener<User>() {
+            @Override
+            public void onSuccess(User newUser) {
+                if (newUser != null) {
+                    user = newUser;
+
+                    if (user.getRoles().isEntrant() || user.getRoles().isOrganizer()){
+                        mainActivityButton.setVisibility(View.VISIBLE);
+                    } else {
+                        mainActivityButton.setVisibility(View.GONE);
+                    }
+                }
+            }
+        }, e -> Log.w("Firebase", "Error getting user", e));
     }
 }
