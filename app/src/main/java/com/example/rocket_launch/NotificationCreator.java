afb1ace88c87ed4c03becb2609be1597b2a2a72b
Kaiden Keane <kjkeane88@gmail.com>
2024-12-01 22:53:21 -0700

    sending fully works

--- app/src/main/java/com/example/rocket_launch/NotificationCreator.java
+++ app/src/main/java/com/example/rocket_launch/NotificationCreator.java
@@ -85,14 +85,16 @@ public class NotificationCreator extends Fragment {
             Notification notification = new Notification(
                     java.util.UUID.randomUUID().toString(), titleText, messageText);
 
-            // send to all users in list
+            // try to send to all users in list
+            int notificationCounter = 0; // to display how many were sent
             for (User user : users) {
                 // if they want to receive notifications, send notification
                 if (user.getNotificationPreferences()) {
                     NotificationHelper.sendPrefabNotification(user.getAndroidId(), notification);
+                    notificationCounter += 1;
                 }
             }
-            Toast.makeText(requireContext(), String.format(Locale.CANADA, "Sent notification to %d users", users.size()), Toast.LENGTH_SHORT).show();
+            Toast.makeText(requireContext(), String.format(Locale.CANADA, "Sent notification to %d users", notificationCounter), Toast.LENGTH_SHORT).show();
             closeFragment();
         }
     }
